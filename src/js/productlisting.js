84c7cdddb980a13e433a2224abba04e88c1423f9
stanleynephi <nul23001@byui.edu>
2025-04-02 15:05:38 +0000

    checkoutform

--- src/js/productlisting.js
+++ src/js/productlisting.js
@@ -1,12 +1,13 @@
 /**function to render the elements of the list */
-import ExternalService from "./ProductData.mjs";
+import ExternalService from "./ExternalServices.mjs";
 import productlist from "./productlist.mjs";
 import loadheaderfooter from "./utils.mjs";
 import { getParams } from "./utils.mjs";
 
-loadheaderfooter();
-const category = getParams("category");
-const productdata = new ExternalService();
-const listelement = document.querySelector(".product-list");
-const productlists = new productlist(category, productdata, listelement);
-productlists.init();
+
+loadheaderfooter()
+const category = getParams("category")
+const productdata = new ExternalService()
+const listelement = document.querySelector(".product-list")
+const productlists = new productlist(category,productdata,listelement)
+productlists.init()
